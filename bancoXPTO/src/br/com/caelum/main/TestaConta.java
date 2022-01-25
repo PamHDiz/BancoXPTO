daef48881c06de2d118e6bad0044fdae1e8b7354
PamHDiz <pamhdiz@gmail.com>
2022-01-24 23:14:29 -0300

    Exercício sobre referências

--- bancoXPTO/src/br/com/caelum/main/TestaConta.java
+++ bancoXPTO/src/br/com/caelum/main/TestaConta.java
@@ -7,7 +7,9 @@ public class TestaConta {
 	public static void main(String[] args) {
 		
 		Conta c1 = new Conta("Danilo", 12345, "0001", 100.0);
-		Conta c2 = new Conta("Danilo", 12345, "0001", 100.0);
+		//Conta c2 = new Conta("Danilo", 12345, "0001", 100.0);
+		
+		Conta c2 = c1;
 		
 		if(c1 == c2) {
 			System.out.println("São iguais");

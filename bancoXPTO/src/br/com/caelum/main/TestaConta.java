268e89830d3885053ef0b111598a6ce45fc08fdc
PamHDiz <pamhdiz@gmail.com>
2022-01-24 23:09:18 -0300

    Criação da classe teste

---
+++ bancoXPTO/src/br/com/caelum/main/TestaConta.java
@@ -0,0 +1,20 @@
+package br.com.caelum.main;
+
+import br.com.caelum.modelo.Conta;
+
+public class TestaConta {
+
+	public static void main(String[] args) {
+		
+		Conta c1 = new Conta("Danilo", 12345, "0001", 100.0);
+		Conta c2 = new Conta("Danilo", 12345, "0001", 100.0);
+		
+		if(c1 == c2) {
+			System.out.println("São iguais");
+		} else {
+			System.out.println("São diferentes");
+		}
+
+	}
+
+}

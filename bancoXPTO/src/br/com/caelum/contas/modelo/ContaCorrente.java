7ca8e67e092f2059bafdd30e71d62147b8c06612
PamHDiz <pamhdiz@gmail.com>
2022-01-28 18:20:31 -0300

    Sobrescrita do método saca

---
+++ bancoXPTO/src/br/com/caelum/contas/modelo/ContaCorrente.java
@@ -0,0 +1,20 @@
+package br.com.caelum.contas.modelo;
+
+public class ContaCorrente extends Conta {
+	
+	@Override
+	public String getTipo() {
+		return "Conta Corrente";
+	}
+	
+	@Override
+	public boolean saca(double valor) {
+		if (valor > this.saldo) {
+			System.out.println("Saldo insuficiente");
+			return false;
+		} else {
+			this.saldo -= (valor + 0.1);
+			return true;
+		}
+	}
+}
---
+++ bancoXPTO/src/br/com/caelum/contas/modelo/ContaPoupanca.java
@@ -0,0 +1,21 @@
+package br.com.caelum.contas.modelo;
+
+public class ContaPoupanca extends Conta {
+	
+	@Override
+	public String getTipo() {
+		return "Conta Poupança";
+	}
+	
+	@Override
+	public boolean saca(double valor) {
+		if (valor > this.saldo) {
+			System.out.println("Saldo insuficiente");
+			return false;
+		} else {
+			this.saldo -= valor;
+			return true;
+		}
+	}
+
+}

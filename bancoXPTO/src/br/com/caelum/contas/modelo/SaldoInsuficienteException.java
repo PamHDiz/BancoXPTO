9298730950c986ff3af8da9ee4f11f69d3011338
PamHDiz <pamhdiz@gmail.com>
2022-02-02 13:33:15 -0300

    Extendendo minha exceção da classe Exception para torná-la checked

--- bancoXPTO/src/br/com/caelum/contas/modelo/SaldoInsuficienteException.java
+++ bancoXPTO/src/br/com/caelum/contas/modelo/SaldoInsuficienteException.java
@@ -1,6 +1,6 @@
 package br.com.caelum.contas.modelo;
 
-public class SaldoInsuficienteException extends RuntimeException {
+public class SaldoInsuficienteException extends Exception {
 	
 		public SaldoInsuficienteException(String message) {
 			super(message);

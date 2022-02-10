2dea11860971927b67e5ee0ecdc1ccc32423769e
PamHDiz <pamhdiz@gmail.com>
2022-02-10 12:11:40 -0300

    Usando o OutPutStream para escrever em um arquivo

---
+++ bancoXPTO/src/br/com/caelum/contas/main/TestaSaida.java
@@ -0,0 +1,22 @@
+package br.com.caelum.contas.main;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class TestaSaida {
+
+	public static void main(String[] args) throws IOException{
+		
+		OutputStream os = new FileOutputStream("TesteIO");
+		OutputStreamWriter osw = new OutputStreamWriter(os);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		bw.write("Vamos ver se funciona");
+		bw.write("\nFuncionou!!!");
+		
+		bw.close();
+	}
+
+}
